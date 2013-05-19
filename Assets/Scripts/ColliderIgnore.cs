61138bc0589e43266852173e239f18c49396ec43
Eric Mellino <raverenx@gmail.com>
2013-05-19 19:37:33 -0400

    changes

---
+++ Assets/Scripts/ColliderIgnore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Causes all colliders on all children to ignore collisions with each other. 
+/// Prevents an object from bumping into itself when moving around.
+/// </summary>
+public class ColliderIgnore : MonoBehaviour
+{
+    void Start()
+    {
+        var allColliders = this.GetComponentsInChildren<Collider>();
+        for (int g = 0; g < allColliders.Length; g++)
+        {
+            for (int i = g + 1; i < allColliders.Length; i++)
+            {
+                if (i != allColliders.Length)
+                    Physics.IgnoreCollision(allColliders[g], allColliders[i], true);
+            }
+        }
+    }
+}
